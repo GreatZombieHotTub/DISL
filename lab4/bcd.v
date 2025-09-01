52e6187ca6ff77ec4b7334e0a379f3560e705bd4
GreatZombieHotTub <94970958+GreatZombieHotTub@users.noreply.github.com>
2025-09-01 12:18:40 +0530

    Update bcd.v

--- lab4/bcd.v
+++ lab4/bcd.v
@@ -14,12 +14,14 @@ assign c = k|(z[3]&z[2])|(z[1]&z[3]);
 adder4 four_bit_adder_instance1(z,{1'b0,c,c,1'b0},s,cin,cout2);
 endmodule
 
-
-module adder4 (carryin, x3, x2, x1, x0, y3, y2, y1, y0, s3, s2, s1, s0, carryout);
-input carryin, x3, x2, x1, x0, y3, y2, y1, y0;
-output s3, s2, s1, s0, carryout;
-full stage0 (carryin, x0, y0, s0, c1);
-full stage1 (c1, x1, y1, s1, c2);
-full stage2 (c2, x2, y2, s2, c3);
-full stage3 (c3, x3, y3, s3, carryout);
+module adder4(a,b,s,cin,cout);
+input [3:0]a,b;
+input cin;
+output [3:0]s;
+output cout;
+wire [3:1] c;
+full stage0(a[0],b[0],cin,s[0],c[1]);
+full stage1(a[1],b[1],c[1],s[1],c[2]);
+full stage2(a[2],b[2],c[2],s[2],c[3]);
+full stage3(a[3],b[3],c[3],s[3],cout);
 endmodule
