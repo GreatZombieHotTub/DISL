55a47abd8f78c01285922bb445be12023b237c87
GreatZombieHotTub <94970958+GreatZombieHotTub@users.noreply.github.com>
2025-09-01 12:12:00 +0530

    Update bcd.v

--- lab4/bcd.v
+++ lab4/bcd.v
@@ -1,7 +1,7 @@
 `include "full.v"
 `timescale 1ns/1ns
 
-module bcd_adder(a,b,cin,s,cout2);
+module bcd(a,b,cin,s,cout2);
 
 input [3:0]a,b;
 input cin;
@@ -9,9 +9,9 @@ output[3:0]s;
 output cout2;
 wire [3:0]z;
 wire k,c;
-four_bit_adder four_bit_adder_instance0(a,b,z,cin,k);
+adder4 four_bit_adder_instance0(a,b,z,cin,k);
 assign c = k|(z[3]&z[2])|(z[1]&z[3]);
-four_bit_adder four_bit_adder_instance1(z,{1'b0,c,c,1'b0},s,cin,cout2);
+adder4 four_bit_adder_instance1(z,{1'b0,c,c,1'b0},s,cin,cout2);
 endmodule
 
 
