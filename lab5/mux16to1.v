78a263880e643c2dab3e3866cb038b91c4d3576c
GreatZombieHotTub <94970958+GreatZombieHotTub@users.noreply.github.com>
2025-09-07 18:21:49 +0530

    Update mux16to1.v

--- lab5/mux16to1.v
+++ lab5/mux16to1.v
@@ -9,5 +9,5 @@ mux4to1 s0(w[3:0],s[1:0],f1);
 mux4to1 s1(w[7:4],s[1:0],f2);
 mux4to1 s2(w[11:8],s[1:0],f3);
 mux4to1 s3(w[15:12],s[1:0],f4);
-mux4to1 s4({f4,f3,f2,f1},s[3:2],f);
+  mux4to1 s4({f1,f2,f3,f4},s[3:2],f);
 endmodule
